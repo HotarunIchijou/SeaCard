7a0c0663154f26137394d6a4262ee7da4cdc9821
Merrcurys <a.sebezhko2017@gmail.com>
2026-03-23 22:04:24 +0300

    ver. 2.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.seacard"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "2.5"
+        versionCode = 12
+        versionName = "2.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
