1bc74f270cd06d594b71b00042d63a292a20338e
Merrcurys <a.sebezhko2017@gmail.com>
2026-04-11 22:41:11 +0300

    3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.seacard"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "2.9"
+        versionCode = 14
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
